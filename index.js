26a6d804cc16a7ec659f25fc77cc59b1ef85fc22
Ademola <adeakanfea@gmailcom>
2024-01-28 19:18:28 +0100

    Second Commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use("/", (req, res) => {
   res.status(200).json({ msg: "Started" });
 });
 
+app.use("/testing", (req, res) => {
+  res.send("Adenola");
+});
+
 const port = process.env.PORT ? process.env.PORT : 4000;
 
 app.listen(port, () => {
